e9cb93494ce650e7e20a7cf4d42f04cc9a164a95
Noah Petrides <nopetrides@gmail.com>
2023-09-29 12:50:13 -0700

    first tutorial

--- src/HelloMurder/HelloMurderGame.cs
+++ src/HelloMurder/HelloMurderGame.cs
@@ -1,9 +1,22 @@
-﻿using Murder;
+﻿using HelloMurder.Core;
+using Microsoft.Xna.Framework.Input;
+using Murder;
+using Murder.Core.Input;
 
 namespace HelloMurder
 {
     public class HelloMurderGame : IMurderGame
     {
         public string Name => "HelloMurder";
+
+        public void Initialize()
+        {
+            Game.Input.RegisterAxes(MurderInputAxis.Movement, GamepadAxis.LeftThumb);
+            Game.Input.Register(MurderInputAxis.Movement, 
+                new InputButtonAxis(Keys.W, Keys.A, Keys.S, Keys.D), 
+                new InputButtonAxis(Keys.Up, Keys.Left, Keys.Down, Keys.Right));
+
+            Game.Input.Register(InputButtons.Interact, Keys.Space);
+        }
     }
 }
